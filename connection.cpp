11eef906d414e9cd378faf08ca66be21a28bced4
hadirmaaroufi <hadir.maaroufi@esprit.com>
2021-04-30 14:27:09 +0100

    H_integration

--- connection.cpp
+++ connection.cpp
@@ -10,9 +10,9 @@ bool Connection::createConnection(){
     bool test=false;
 
     QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-    db.setDatabaseName("Project");
-    db.setUserName("omar");
-    db.setPassword("system");
+    db.setDatabaseName("Projet");
+    db.setUserName("hadir");
+    db.setPassword("hadir");
 
     if (db.open())
         test=true;
